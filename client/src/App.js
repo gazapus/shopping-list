527d923a8282c1d3fb2d0b67a64d2d5fa247c971
sendrarisk <cristianv129@hotmail.com>
2020-07-11 14:33:18 -0300

    Agregado estado y modificado estilo de App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import TopAppBar from './components/TopAppBar';
 import BottonBar from './components/BottonBar';
 import InputItem from './components/InputItem';
 
-
-const useStyles = makeStyles({
+const useStyles = theme => ({
   root: {
     background: '#F7E4B4',
     borderRadius: 3,
@@ -16,18 +15,29 @@ const useStyles = makeStyles({
     margin: 0,
     padding: 0,
     paddingBottom: "9vh"
-  }
+   }
 });
 
-function App() {
-  const classes = useStyles();
-  return (
-    <Container maxWidth="sm" classes={{ root: classes.root}}>
-      <TopAppBar title="Nueva List" />
-      <InputItem/>
-      <BottonBar />
-    </Container>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+          this.state = {
+               itemsLoaded: [],
+               itemsNotLoaded: [],
+               listName: "Nueva Lista",
+               accumulatedAmount: 0
+          };
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container maxWidth="sm" className={classes.root}>
+        <TopAppBar title={this.state.listName} />
+        <InputItem />
+        <BottonBar />
+      </Container>
+    );
+  }
 }
 
-export default App;
+export default withStyles(useStyles)(App)
