ab59e32d70ba54f2ebf212dabc4c1bb562d61e17
sendrarisk <cristianv129@hotmail.com>
2020-07-24 18:41:38 -0300

    backend conectado al servidor

---
+++ keys.js
@@ -0,0 +1,5 @@
+const keys = {
+    mongoUri: 'mongodb+srv://gazapus:Fati2511@cluster0.yjnbb.mongodb.net/'
+}
+
+module.exports =  keys;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 const bodyParser = require("body-parser");
+const keys = require('./keys');
+
+const client = new MongoClient(keys.mongoUri, { useUnifiedTopology: true });
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,64 +13,48 @@ app.get('/', function (req, res) {
 });
 
 app.post('/save', async (req, res) => {
-  MongoClient.connect('mongodb://localhost:27017/', { useUnifiedTopology: true }, async (err, client) => {
-    var db = client.db('shoplist');
-    if (err) {
-      throw err;
-    }
-    try {
-      await db.collection("lists").replaceOne({ name: req.body.name }, req.body, { upsert: true });
-      res.sendStatus(200);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
-  });
+  await client.connect();
+  var db = client.db('shoppinglist');
+  try {
+    await db.collection("lists").replaceOne({ name: req.body.name }, req.body, { upsert: true });
+    res.sendStatus(200);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 app.get('/open/:name', async (req, res) => {
-  MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, async (err, client) => {
-    var db = client.db('shoplist');
-    if (err) {
-      throw err;
-    }
-    var result = await db.collection('lists').findOne({ name: req.params.name });
-    res.send(result);
-  });
+  await client.connect();
+  var db = client.db('shoppinglist');
+  var result = await db.collection('lists').findOne({ name: req.params.name });
+  res.send(result);
 });
 
 app.delete('/delete/:name', async (req, res) => {
-  MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, async (err, client) => {
-    var db = client.db('shoplist');
-    if (err) {
-      throw err;
-    }
-    try {
-      await db.collection('lists').deleteOne({ name: req.params.name });
-      res.sendStatus(200);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
-  });
+  await client.connect();
+  var db = client.db('shoppinglist');
+  try {
+    await db.collection('lists').deleteOne({ name: req.params.name });
+    res.sendStatus(200);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 app.get('/all', async (req, res) => {
-  MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, async (err, client) => {
-    var db = client.db('shoplist');
-    if (err) {
-      throw err;
-    }
-    try {
-      let result = await db.collection('lists').find({}).toArray()
-      res.send(result);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
-  });
+  await client.connect();
+  var db = client.db('shoppinglist');
+  try {
+    let result = await db.collection('lists').find({}).toArray()
+    res.send(result);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
+  console.log('Server listening on port 8000!');
 });
